4a6013b4d582a2a37e6733a49dd48dda3621c3f2
lourenxx <guilhermepexiloureco@gmail.com>
2026-04-15 21:36:56 -0300

    First commit - Project Environment

---
+++ src/main/java/br/edu/faculdade/sistemapastelaria/SistemaPastelariaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.faculdade.sistemapastelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPastelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPastelariaApplication.class, args);
+	}
+
+}
