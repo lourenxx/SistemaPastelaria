eab9c358a8858d156bfc7a04953ac85229cf666e
lourenxx <guilhermepexiloureco@gmail.com>
2026-04-23 23:28:52 -0300

    Implements UsuarioController listarUsuario

--- src/main/java/br/edu/faculdade/sistemapastelaria/controller/UsuarioController.java
+++ src/main/java/br/edu/faculdade/sistemapastelaria/controller/UsuarioController.java
@@ -1,10 +1,28 @@
 package br.edu.faculdade.sistemapastelaria.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.faculdade.sistemapastelaria.dto.UsuarioDTO;
+import br.edu.faculdade.sistemapastelaria.service.UsuarioService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<UsuarioDTO> pesquisarTodos() {
+        return usuarioService.listarUsuarios();
+    }
 
 }
